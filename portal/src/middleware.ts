7a89a21e5f715e9e7f816be915a8c5f839e0a5aa
Tex Murphy <dxutakerxd@gmail.com>
2025-10-08 10:55:47 -0400

    Update middleware.ts

--- portal/src/middleware.ts
+++ portal/src/middleware.ts
@@ -1,17 +1,20 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  // An array of routes that should be accessible to both signed-in and signed-out users
-  publicRoutes: ['/', '/sign-in', '/sign-up'],
-  // Enable debug mode to get detailed authentication logs
-  debug: true,
-})
+// Define the routes that should be accessible to both signed-in and signed-out users
+const isPublicRoute = createRouteMatcher(['/', '/sign-in', '/sign-up', '/api/signup-lead']);
+
+export default clerkMiddleware((auth, req) => {
+  // Protect all routes that are not public
+  if (!isPublicRoute(req)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
