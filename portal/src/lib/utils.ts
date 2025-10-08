6f50f526be470cfd203db751b19b9857d4709223
Tex Murphy <dxutakerxd@gmail.com>
2025-10-08 10:32:35 -0400

    Update utils.ts

--- portal/src/lib/utils.ts
+++ portal/src/lib/utils.ts
@@ -5,3 +5,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function absoluteUrl(path: string) {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000';
+  return `${baseUrl}${path}`;
+}
