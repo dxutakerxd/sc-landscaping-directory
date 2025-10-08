49767faea73ac6e47c01cba77f718baa34e60e1b
Tex Murphy <dxutakerxd@gmail.com>
2025-10-08 11:15:49 -0400

    downgrade
    major downgrade

--- portal/src/middleware.ts
+++ portal/src/middleware.ts
@@ -1,20 +1,13 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { authMiddleware } from "@clerk/nextjs";
 
-// Define the routes that should be accessible to both signed-in and signed-out users
-const isPublicRoute = createRouteMatcher(['/', '/sign-in', '/sign-up', '/api/signup-lead']);
-
-export default clerkMiddleware((auth, req) => {
-  // Protect all routes that are not public
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
+export default authMiddleware({
+  publicRoutes: ['/', '/api/signup-lead'],
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ['/((?!.+\.[\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
 };
